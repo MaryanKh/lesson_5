02d502ce4ae0cc37e1daeaa9d6a581d1bb37b049
MaryanKh <mar9yan@gmail.com>
2018-11-12 16:15:14 +0200

    add lesson_5

---
+++ lesson_5/Assets/Scripts/GenerateElements.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateElements : MonoBehaviour {
+
+    public GameObject prefab;
+    private Vector3 spawnCoords = Vector3.zero;
+    public GameObject player;
+    private const float planeLength = 50.0f;
+
+    private List<GameObject> planes = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        for ( int i = 0; i < 10000; i++) {
+            GameObject go = Instantiate(prefab, spawnCoords, Quaternion.identity, gameObject.transform) as GameObject;
+            planes.Add(go);
+            spawnCoords.x += planeLength;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if((player.transform.position.x + planeLength) > spawnCoords.x) {
+            spawnCoords.x += planeLength;
+            GameObject go = Instantiate(prefab, spawnCoords, Quaternion.identity, gameObject.transform) as GameObject;
+            planes.Add(go);
+        }
+
+        if(planes[0].transform.position.x < (player.transform.position.x - planeLength)) {
+            Destroy(planes[0]);
+            planes.RemoveAt(0);
+        }
+	}
+}
---
+++ lesson_5/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float jump = 3.0f;
+    private CharacterController cc;
+    private Vector3 move, gravity;
+    public int score = 0;
+    public Text countText, loseText;
+
+    private void Start() {
+        cc = GetComponent<CharacterController>();
+        move = Vector3.zero;
+        gravity = Vector3.zero;
+        loseText.text = "";
+        Count();
+    }
+
+    private void Update() {
+        if(Input.GetButton("Horizontal")) {
+            move.z = -Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        }
+        if(cc.isGrounded)
+        {
+            gravity = Vector3.zero;
+            if (Input.GetButton("Jump"))
+            {
+                gravity.y = jump;
+            }
+        }
+        else
+        {
+            gravity += Physics.gravity * Time.deltaTime * 3;
+        }
+        
+        move.x = speed * Time.deltaTime;
+        move += gravity;
+        move *= Time.deltaTime;
+        cc.Move(move);
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Coin")
+        {
+            Destroy(hit.gameObject);
+            score++;
+            Count();
+        }
+
+        if(hit.gameObject.tag == "Barricade")
+        {
+            loseText.text = "You lose!";
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void Count()
+    {
+        countText.text = "Count: " + score.ToString();
+    }
+}
